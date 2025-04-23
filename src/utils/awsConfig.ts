413cba804ccc1b7e13c0b86190f5d541bc247a92
Roberto V <114187164+RoberticoV@users.noreply.github.com>
2025-04-22 21:59:41 -0400

    Deleted credentials

--- src/utils/awsConfig.ts
+++ src/utils/awsConfig.ts
@@ -2,8 +2,8 @@ export const AWS_CONFIG = {
   region: "us-east-1",
   credentials: {
     // In a real application, these should be environment variables
-    accessKeyId: "AKIAQSOI4IMTA5WZSSO7",
-    secretAccessKey: "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"
+    accessKeyId: "",
+    secretAccessKey: ""
   },
   tableName: "Predictions_1"
 };
