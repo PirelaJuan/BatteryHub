25dcf7c9b662ca89214c6150b028daa2044655ea
PirelaJuan <pirelajuan13@gmail.com>
2025-09-02 13:54:14 -0400

    Commented secrets

--- src/utils/awsConfig.ts
+++ src/utils/awsConfig.ts
@@ -2,8 +2,8 @@ export const AWS_CONFIG = {
   region: "us-east-1",
   credentials: {
     // In a real application, these should be environment variables
-    accessKeyId: "",
-    secretAccessKey: ""
+   // accessKeyId: "",
+    //secretAccessKey: ""
   },
   tableName: "Predictions_1"
 };
